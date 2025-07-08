4144bd9413864e21f857efba75694611c0b76f18
ShokhrukhDotNet <zokirjonovshoxrux99@gmail.com>
2025-07-08 11:01:11 +0500

    INFRA: Initialize Unit Test Project

---
+++ MetaXPorter.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use For Reliable File Conversion
+//==================================================
+
+namespace MetaXPorter.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
